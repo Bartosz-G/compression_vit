c4cb2f9a547d415b437c683e8ffcd69f48c08846
Bartosz Gawin <bartex.gw@gmail.com>
2024-04-22 13:57:01 +0100

    cleanup

--- model/init.py
+++ model/init.py
@@ -8,14 +8,12 @@ from contextlib import contextmanager
 
 @contextmanager
 def set_seed(seed):
-    # Save the state of the random number generators
     state_torch = torch.random.get_rng_state()
     if torch.cuda.is_available():
         state_cuda = torch.cuda.random.get_rng_state()
     state_np = np.random.get_state()
     state_random = random.getstate()
 
-    # Set the new seed temporarily
     torch.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
@@ -23,10 +21,8 @@ def set_seed(seed):
         torch.cuda.manual_seed_all(seed)
 
     try:
-        # Yield control to the block of code
         yield
     finally:
-        # Restore the original random number generator states
         torch.random.set_rng_state(state_torch)
         if torch.cuda.is_available():
             torch.cuda.random.set_rng_state(state_cuda)
@@ -44,7 +40,7 @@ def truncated_normal_(tensor, mean=0, std=1, trunc_std=2):
         std (float): Standard deviation of the normal distribution.
         trunc_std (float): Number of standard deviations within which to truncate.
     """
-    # Calculate the upper and lower bound.
+
     with torch.no_grad():
         size = tensor.size()
         tmp = tensor.new_empty(size + (4,)).normal_()
@@ -62,7 +58,6 @@ def init_truncated_normal(m):
         if m.bias is not None:
             nn.init.constant_(m.bias, 0.0)
     elif isinstance(m, nn.Parameter):
-        # Optional: Initialize only if Parameter has more than 1 dimension (common for weights)
         if m.requires_grad and len(m.shape) > 1:
             truncated_normal_(m.data, mean=0.0, std=0.01)
 
@@ -73,12 +68,9 @@ def init_normal(m):
     Also initialize nn.Parameter with normal distribution if detected.
     """
     if isinstance(m, nn.Linear):
-        # Initialize weights with normal distribution
         nn.init.normal_(m.weight, mean=0.0, std=0.01)
-        # Initialize biases to zero
         if m.bias is not None:
             nn.init.constant_(m.bias, 0.0)
     elif isinstance(m, nn.Parameter):
-        # Check if the parameter requires grad and isn't a bias (optional check)
         if m.requires_grad and len(m.shape) > 1:
             nn.init.normal_(m.data, mean=0.0, std=0.01)
