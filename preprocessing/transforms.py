32ae1b9a25ddc4a4a4d6d6b43a370e5b7c1fe6be
Daniel Warfield <danielwarfield1@gmail.com>
2024-06-24 22:14:01 +0000

    undo test commit

--- preprocessing/transforms.py
+++ preprocessing/transforms.py
@@ -2,7 +2,6 @@ import torch
 import numpy as np
 from scipy.fft import dctn, idctn
 from torchvision.transforms import functional
-#test commit
 
 LUMINANCE_QUANTIZATION_MATRIX = np.array([
     [16, 11, 10, 16, 24, 40, 51, 61],
